7609f42c9b8f26abe3b9607f9f54e2990993962f
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-31 11:42:30 +0530

    Add log in the Demoaction

--- actions/demoaction.ts
+++ actions/demoaction.ts
@@ -1,5 +1,6 @@
 'use server';
 export const demoAction = ()=>{
+    console.log(process.env.MY_NUMBER);
     const num = Number(process.env.MY_NUMBER);
     console.log("The number from env is ---> ",num);
     return num;
