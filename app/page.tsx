74b8d42d5921bd32dc2331e50f3319a6f600e1b5
Vishnu Kumar <vishnukumart96@gmail.com>
2024-03-31 11:25:12 +0530

    Make the page a Client Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+'use client'
 import { demoAction } from "@/actions/demoaction";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const num = demoAction();
+  const[num,setNum] = useState(11111111);
+  useEffect(()=>{
+    getData();
+  },[])
+
+  async function getData(){
+    const n = demoAction();
+    setNum((p)=>n);
+    console.log(num);
+  }
+  
   return (
     <>
-      The num from DemoAction is `{num}`
+      The num from DemoAction is {num}
     </>
   );
 }
